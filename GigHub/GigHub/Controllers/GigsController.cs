e775100bd7ab951eabbfa4645107059ce5bdb19d
Miguel Costa <miguelpintodacosta@gmail.com>
2016-07-02 15:14:53 +0100

    Add Gigs controller

---
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public string Venue { get; set; }
+    }
+}
