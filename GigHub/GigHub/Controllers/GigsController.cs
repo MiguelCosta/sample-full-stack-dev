688456dbc4d5ac865dd7e2d7207d6e8f6d0e6240
Miguel Costa <miguelpintodacosta@gmail.com>
2016-07-15 01:58:42 +0100

    Create Gig action

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -1,4 +1,6 @@
 ﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -13,14 +15,36 @@ namespace GigHub.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
-
             var viewModel = new ViewModels.GigFormViewModel
             {
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(ViewModels.GigFormViewModel viewModel)
+        {
+            var userId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == userId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+
+            var gig = new Gig
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse($"{viewModel.Date} {viewModel.Time}"),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
