c4e99edd2f7b4dfb490cf023780789cc200c49b8
Miguel Costa <miguelpintodacosta@gmail.com>
2016-07-22 23:57:06 +0100

    Add gigs style

--- GigHub/GigHub/Controllers/HomeController.cs
+++ GigHub/GigHub/Controllers/HomeController.cs
@@ -21,6 +21,7 @@ namespace GigHub.Controllers
         {
             var upCommingGigs = _context.Gigs
                 .Include(g => g.Artist)
+                .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
             return View(upCommingGigs);
         }
