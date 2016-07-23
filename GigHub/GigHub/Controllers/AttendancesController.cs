2b4320a89402a2c70c13a91d6462eefe312d9a51
Miguel Costa <miguelpintodacosta@gmail.com>
2016-07-23 15:54:40 +0100

    Register attendance

---
+++ GigHub/GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,43 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Attend([FromBody] AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var exist = _context.Attendances
+                            .Any(a => a.AttendeeId == userId && a.GigId == dto.GigId);
+
+            if(exist)
+            {
+                return BadRequest("The attendance already exists.");
+            }
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+            _context.Attendances.Add(attendance);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ GigHub/GigHub/Dtos/AttendanceDto.cs
@@ -0,0 +1,7 @@
+﻿namespace GigHub.Dtos
+{
+    public class AttendanceDto
+    {
+        public int GigId { get; set; }
+    }
+}
