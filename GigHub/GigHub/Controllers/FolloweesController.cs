b4b598f5c0caa3f50fdd039f6f760e1339bbc9ab
Miguel Costa <miguelpintodacosta@gmail.com>
2016-07-23 17:12:53 +0100

    Add follwing page

---
+++ GigHub/GigHub/Controllers/FolloweesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Followees
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var artists = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(f => f.Followee);
+
+            return View(artists);
+        }
+    }
+}
